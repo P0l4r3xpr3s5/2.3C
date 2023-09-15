cd891d5aa079889590466e07f28e291fae99488f
P0l4r3xpr3s5 <141984452+P0l4r3xpr3s5@users.noreply.github.com>
2023-09-15 23:36:13 +1000

    App.vue

---
+++ App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <!-- 11. Router -->
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/products">Products</router-link>
+      <router-link to="/cart">Cart</router-link>
+      <router-link to="/checkout">Checkout</router-link>
+    </nav>
+    <!-- 11. Router -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script> 
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+</style>
