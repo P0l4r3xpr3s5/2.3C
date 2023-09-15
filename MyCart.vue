91f02abc30894a317e9bea4c23438778acb9fa47
P0l4r3xpr3s5 <141984452+P0l4r3xpr3s5@users.noreply.github.com>
2023-09-15 23:34:42 +1000

    MyCart.vue

---
+++ MyCart.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Cart</h1>
+        <!-- 6.a v-for with an Object -->
+        <ul>
+            <li v-for="item in cart" :key="item.id">{{ item.name }}</li>
+        </ul>
+        <!-- 11. Router -->
+        <router-link to="/checkout">Proceed to Checkout</router-link>
+    </div>
+</template>
+
+<script setup>
+// 3. Reactivity Fundamentals [ref(), <script setup>]
+import { ref } from 'vue'
+
+const cart = ref([])
+</script>
